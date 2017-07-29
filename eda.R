f82dcf500b6a0a3e7bdc0fffe14a3db45636c7a6
vincentvangoat <menogetusername@gmail.com>
2017-07-29 14:40:57 -0400

    eda v0

---
+++ eda.R
@@ -0,0 +1,15 @@
+setwd("FWYF-hack/")
+library(tidytext)
+library(tidyverse)
+library(data.table)
+
+lyrics <- as.data.frame(fread("lyrics.csv", sep = ","))
+
+lyric_words <- lyrics %>%
+  unnest_tokens(word, lyrics) %>%
+  group_by(song) %>%
+  mutate(word_position = row_number()/n())
+
+
+
+# lyric_words <-
